2404d9a92abd62966bc45575e730c19a60a1dcfb
Natallia Petrachenka <natallia.petrachenka@softeq.com>
2025-02-23 13:55:11 +0100

    Init project

---
+++ iAppDemo/iAppDemo/iAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAppDemoApp.swift
+//  iAppDemo
+//
+//  Created by Natallia Petrachenka on 23.02.25.
+//
+
+import SwiftUI
+
+@main
+struct iAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iAppDemo/iAppDemoTests/iAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  iAppDemoTests.swift
+//  iAppDemoTests
+//
+//  Created by Natallia Petrachenka on 23.02.25.
+//
+
+import Testing
+@testable import iAppDemo
+
+struct iAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
